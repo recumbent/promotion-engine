8e2a4f3cb0766e14944b08c4987b83779d534ff8
Murph <james@recumbent.co.uk>
2021-04-14 17:59:59 +0100

    More tests for nothing to apply the offer to

--- PromotionEngine.Tests/MultibuyTests.cs
+++ PromotionEngine.Tests/MultibuyTests.cs
@@ -12,10 +12,36 @@ namespace PromotionEngine.Tests
     {
         // Assume 3 of A's for 130
 
+
+        // TODO: Ideally this would be a Theory (data driven) test for "don't apply" cases, need to see where we go first
         [Fact]
-        public void ShouldApplShouldReturnFalseIfBasketIsEmpty()
+        public void ShouldApplyShouldReturnFalseIfBasketIsEmpty()
         {
             var basket = new List<BasketItem>();
+            var result = Multibuy.ShouldApplyPromotion(basket);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldApplyShouldReturnFalseIfBasketHasTwoAs()
+        {
+            var basket = new List<BasketItem>()
+            { new BasketItem("A", 2, 50) };
+            var result = Multibuy.ShouldApplyPromotion(basket);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldApplyShouldReturnFalseIfBasketHasWrongItemTypes()
+        {
+            var basket = new List<BasketItem>()
+            {
+                new BasketItem("B", 2, 30),
+                new BasketItem("C", 3, 20), 
+                new BasketItem("D", 4, 15) 
+            };
             var result = Multibuy.ShouldApplyPromotion(basket);
 
             result.Should().BeFalse();
